100062e4e5c29c0be1f417c4f3daf8efc858d3ab
Yohann MARTIN <mimichackeur@gmail.com>
2023-12-01 18:15:38 +0100

    Better code golf version with list instead of dict

--- src/day1/part2.py
+++ src/day1/part2.py
@@ -4,23 +4,13 @@ contents = Path(Path(__file__).parent, "input").read_text()
 
 
 def transform_spelling(line: str) -> list[int]:
-    spellings = {
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9,
-    }
+    spellings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     numbers: list[int] = []
 
     for i in range(len(line)):
-        for spell in spellings.keys():
-            if line[i:].startswith(spell):
-                numbers.append(spellings[spell])
+        for j in range(len(spellings)):
+            if line[i:].startswith(spellings[j]):
+                numbers.append(j + 1)
                 break
         if line[i].isdigit():
             numbers.append(int(line[i]))
@@ -37,6 +27,6 @@ print(f"Result: {sum_calib}")  # Result: 54265
 
 # One liner (code golf for fun)
 # fmt: off
-spellings = {"one": 1,"two": 2,"three": 3,"four": 4,"five": 5,"six": 6,"seven": 7,"eight": 8,"nine": 9}
-print(sum([(n[0]*10)+n[-1]for n in[[int(L[i])if L[i].isdigit()else spellings[[s for s in spellings if L[i:].startswith(s)][0]]for i in range(len(L))if L[i].isdigit() or any(L[i:].startswith(s) for s in spellings)] for L in open("input")]]))
+p=["one","two","three", "four","five","six","seven","eight","nine"]
+print(sum([(n[0]*10)+n[-1]for n in[[int(L[i])if L[i].isdigit()else[j+1for j in range(len(p))if L[i:].startswith(p[j])][0]for i in range(len(L))if L[i].isdigit()or any(L[i:].startswith(s) for s in p)]for L in open("input")]]))
 # fmt: on
