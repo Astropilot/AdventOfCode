b7030d4488912b43d21b36d4c1640e85f0cbb457
Yohann MARTIN <mimichackeur@gmail.com>
2023-12-05 23:48:55 +0100

    Code cleanup

--- src/2023/day5/part1.py
+++ src/2023/day5/part1.py
@@ -12,122 +12,31 @@ light_to_temperature_mapping: list[tuple[int, int, int]] = []
 temperature_to_humidity_mapping: list[tuple[int, int, int]] = []
 humidity_to_location_mapping: list[tuple[int, int, int]] = []
 
+
+def read_mapping(
+    line_pos: int, lines: list[str], mapping: list[tuple[int, int, int]]
+) -> int:
+    line_pos += 1
+    while line_pos < len(lines) and len(lines[line_pos]):
+        mapping_line = list(map(int, lines[line_pos].split(" ")))
+        mapping.append(
+            (mapping_line[1], mapping_line[1] + (mapping_line[2] - 1), mapping_line[0])
+        )
+        line_pos += 1
+
+    line_pos += 1
+    return line_pos
+
+
 line_pos = 2
 
-assert lines[line_pos] == "seed-to-soil map:"
-
-line_pos += 1
-while len(lines[line_pos]):
-    seed_to_soil = list(map(int, lines[line_pos].split(" ")))
-    seed_to_soil_mapping.append(
-        (seed_to_soil[1], seed_to_soil[1] + (seed_to_soil[2] - 1), seed_to_soil[0])
-    )
-    line_pos += 1
-
-line_pos += 1
-
-assert lines[line_pos] == "soil-to-fertilizer map:"
-
-line_pos += 1
-while len(lines[line_pos]):
-    soil_to_fertilizer = list(map(int, lines[line_pos].split(" ")))
-    soil_to_fertilizer_mapping.append(
-        (
-            soil_to_fertilizer[1],
-            soil_to_fertilizer[1] + (soil_to_fertilizer[2] - 1),
-            soil_to_fertilizer[0],
-        )
-    )
-    line_pos += 1
-
-line_pos += 1
-
-assert lines[line_pos] == "fertilizer-to-water map:"
-
-line_pos += 1
-while len(lines[line_pos]):
-    fertilizer_to_water = list(map(int, lines[line_pos].split(" ")))
-    fertilizer_to_water_mapping.append(
-        (
-            fertilizer_to_water[1],
-            fertilizer_to_water[1] + (fertilizer_to_water[2] - 1),
-            fertilizer_to_water[0],
-        )
-    )
-    line_pos += 1
-
-line_pos += 1
-
-assert lines[line_pos] == "water-to-light map:"
-
-line_pos += 1
-while len(lines[line_pos]):
-    water_to_light = list(map(int, lines[line_pos].split(" ")))
-    water_to_light_mapping.append(
-        (
-            water_to_light[1],
-            water_to_light[1] + (water_to_light[2] - 1),
-            water_to_light[0],
-        )
-    )
-    line_pos += 1
-
-line_pos += 1
-
-assert lines[line_pos] == "light-to-temperature map:"
-
-line_pos += 1
-while len(lines[line_pos]):
-    light_to_temperature = list(map(int, lines[line_pos].split(" ")))
-    light_to_temperature_mapping.append(
-        (
-            light_to_temperature[1],
-            light_to_temperature[1] + (light_to_temperature[2] - 1),
-            light_to_temperature[0],
-        )
-    )
-    line_pos += 1
-
-line_pos += 1
-
-assert lines[line_pos] == "temperature-to-humidity map:"
-
-line_pos += 1
-while len(lines[line_pos]):
-    temperature_to_humidity = list(map(int, lines[line_pos].split(" ")))
-    temperature_to_humidity_mapping.append(
-        (
-            temperature_to_humidity[1],
-            temperature_to_humidity[1] + (temperature_to_humidity[2] - 1),
-            temperature_to_humidity[0],
-        )
-    )
-    line_pos += 1
-
-line_pos += 1
-
-assert lines[line_pos] == "humidity-to-location map:"
-
-line_pos += 1
-while line_pos < len(lines):
-    humidity_to_location = list(map(int, lines[line_pos].split(" ")))
-    humidity_to_location_mapping.append(
-        (
-            humidity_to_location[1],
-            humidity_to_location[1] + (humidity_to_location[2] - 1),
-            humidity_to_location[0],
-        )
-    )
-    line_pos += 1
-
-# print("seeds", seeds)
-# print("seed_to_soil", seed_to_soil_mapping)
-# print("soil_to_fertilizer", soil_to_fertilizer_mapping)
-# print("fertilizer_to_water", fertilizer_to_water_mapping)
-# print("water_to_light", water_to_light_mapping)
-# print("light_to_temperature", light_to_temperature_mapping)
-# print("temperature_to_humidity", temperature_to_humidity_mapping)
-# print("humidity_to_location", humidity_to_location_mapping)
+line_pos = read_mapping(line_pos, lines, seed_to_soil_mapping)
+line_pos = read_mapping(line_pos, lines, soil_to_fertilizer_mapping)
+line_pos = read_mapping(line_pos, lines, fertilizer_to_water_mapping)
+line_pos = read_mapping(line_pos, lines, water_to_light_mapping)
+line_pos = read_mapping(line_pos, lines, light_to_temperature_mapping)
+line_pos = read_mapping(line_pos, lines, temperature_to_humidity_mapping)
+line_pos = read_mapping(line_pos, lines, humidity_to_location_mapping)
 
 
 def get_destination_from_mapping(
@@ -139,7 +48,7 @@ def get_destination_from_mapping(
     return source
 
 
-locations: list[int] = []
+min_location = -1
 
 for seed in seeds:
     soil = get_destination_from_mapping(seed, seed_to_soil_mapping)
@@ -152,16 +61,7 @@ for seed in seeds:
     )
     location = get_destination_from_mapping(humidity, humidity_to_location_mapping)
 
-    locations.append(location)
+    if min_location == -1 or location < min_location:
+        min_location = location
 
-    # print(f"seed {seed}")
-    # print(f"\tsoil: {soil}")
-    # print(f"\tfertilizer: {fertilizer}")
-    # print(f"\twater: {water}")
-    # print(f"\tlight: {light}")
-    # print(f"\ttemperature: {temperature}")
-    # print(f"\thumidity: {humidity}")
-    # print(f"\tlocation: {location}")
-
-print(locations)
-print(f"Result: {min(locations)}")
+print(f"Result: {min_location}")  # Result: 379811651
