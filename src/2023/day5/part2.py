b8d9134be86b755d3705aaa32306fbab3184a113
Yohann MARTIN <mimichackeur@gmail.com>
2023-12-05 23:38:10 +0100

    Code cleanup again

--- src/2023/day5/part2.py
+++ src/2023/day5/part2.py
@@ -18,113 +18,51 @@ light_to_temperature_mapping: list[tuple[int, int, int]] = []
 temperature_to_humidity_mapping: list[tuple[int, int, int]] = []
 humidity_to_location_mapping: list[tuple[int, int, int]] = []
 
+
+def read_mapping(
+    line_pos: int, lines: list[str], mapping: list[tuple[int, int, int]]
+) -> int:
+    line_pos += 1
+    while line_pos < len(lines) and len(lines[line_pos]):
+        mapping_line = list(map(int, lines[line_pos].split(" ")))
+        mapping.append(
+            (mapping_line[1], mapping_line[1] + (mapping_line[2] - 1), mapping_line[0])
+        )
+        line_pos += 1
+
+    line_pos += 1
+    return line_pos
+
+
 line_pos = 2
 
 assert lines[line_pos] == "seed-to-soil map:"
 
-line_pos += 1
-while len(lines[line_pos]):
-    seed_to_soil = list(map(int, lines[line_pos].split(" ")))
-    seed_to_soil_mapping.append(
-        (seed_to_soil[1], seed_to_soil[1] + (seed_to_soil[2] - 1), seed_to_soil[0])
-    )
-    line_pos += 1
-
-line_pos += 1
+line_pos = read_mapping(line_pos, lines, seed_to_soil_mapping)
 
 assert lines[line_pos] == "soil-to-fertilizer map:"
 
-line_pos += 1
-while len(lines[line_pos]):
-    soil_to_fertilizer = list(map(int, lines[line_pos].split(" ")))
-    soil_to_fertilizer_mapping.append(
-        (
-            soil_to_fertilizer[1],
-            soil_to_fertilizer[1] + (soil_to_fertilizer[2] - 1),
-            soil_to_fertilizer[0],
-        )
-    )
-    line_pos += 1
-
-line_pos += 1
+line_pos = read_mapping(line_pos, lines, soil_to_fertilizer_mapping)
 
 assert lines[line_pos] == "fertilizer-to-water map:"
 
-line_pos += 1
-while len(lines[line_pos]):
-    fertilizer_to_water = list(map(int, lines[line_pos].split(" ")))
-    fertilizer_to_water_mapping.append(
-        (
-            fertilizer_to_water[1],
-            fertilizer_to_water[1] + (fertilizer_to_water[2] - 1),
-            fertilizer_to_water[0],
-        )
-    )
-    line_pos += 1
-
-line_pos += 1
+line_pos = read_mapping(line_pos, lines, fertilizer_to_water_mapping)
 
 assert lines[line_pos] == "water-to-light map:"
 
-line_pos += 1
-while len(lines[line_pos]):
-    water_to_light = list(map(int, lines[line_pos].split(" ")))
-    water_to_light_mapping.append(
-        (
-            water_to_light[1],
-            water_to_light[1] + (water_to_light[2] - 1),
-            water_to_light[0],
-        )
-    )
-    line_pos += 1
-
-line_pos += 1
+line_pos = read_mapping(line_pos, lines, water_to_light_mapping)
 
 assert lines[line_pos] == "light-to-temperature map:"
 
-line_pos += 1
-while len(lines[line_pos]):
-    light_to_temperature = list(map(int, lines[line_pos].split(" ")))
-    light_to_temperature_mapping.append(
-        (
-            light_to_temperature[1],
-            light_to_temperature[1] + (light_to_temperature[2] - 1),
-            light_to_temperature[0],
-        )
-    )
-    line_pos += 1
-
-line_pos += 1
+line_pos = read_mapping(line_pos, lines, light_to_temperature_mapping)
 
 assert lines[line_pos] == "temperature-to-humidity map:"
 
-line_pos += 1
-while len(lines[line_pos]):
-    temperature_to_humidity = list(map(int, lines[line_pos].split(" ")))
-    temperature_to_humidity_mapping.append(
-        (
-            temperature_to_humidity[1],
-            temperature_to_humidity[1] + (temperature_to_humidity[2] - 1),
-            temperature_to_humidity[0],
-        )
-    )
-    line_pos += 1
-
-line_pos += 1
+line_pos = read_mapping(line_pos, lines, temperature_to_humidity_mapping)
 
 assert lines[line_pos] == "humidity-to-location map:"
 
-line_pos += 1
-while line_pos < len(lines):
-    humidity_to_location = list(map(int, lines[line_pos].split(" ")))
-    humidity_to_location_mapping.append(
-        (
-            humidity_to_location[1],
-            humidity_to_location[1] + (humidity_to_location[2] - 1),
-            humidity_to_location[0],
-        )
-    )
-    line_pos += 1
+line_pos = read_mapping(line_pos, lines, humidity_to_location_mapping)
 
 
 def intervals_to_mapped_groups(
