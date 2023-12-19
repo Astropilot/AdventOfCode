8e094291637426aa0da7d8e98a03f75646955647
Yohann MARTIN <mimichackeur@gmail.com>
2023-12-19 23:20:20 +0100

    Added code for Day 19

---
+++ 2023/day19/part1.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+
+contents = Path(Path(__file__).parent, "input").read_text()
+workflows_lines, parts_lines = contents.split("\n\n")
+
+workflows: dict[str, list[tuple[str | None, str]]] = {}
+
+for wf_line in workflows_lines.split("\n"):
+    label, rules_line = wf_line.split("{")
+    rules_line = rules_line[:-1]
+    rules_raw = rules_line.split(",")
+    rules: list[tuple[str | None, str]] = []
+
+    for r in rules_raw:
+        data = r.split(":")
+
+        if len(data) == 2:
+            rules.append((data[0], data[1]))
+        else:
+            rules.append((None, data[0]))
+
+    workflows[label] = rules
+
+total_parts = 0
+for ratings in parts_lines.split("\n"):
+    rating: dict[str, int] = {}
+
+    for t in ratings[1:-1].split(","):
+        rating[t[0]] = int(t[2:])
+
+    status = None
+    current_workflow = workflows["in"]
+
+    while status is None:
+        for rule in current_workflow:
+            if rule[0] is None:
+                if rule[1] in ["A", "R"]:
+                    status = rule[1]
+                    break
+                else:
+                    current_workflow = workflows[rule[1]]
+                    break
+            else:
+                ra = rating[rule[0][0]]
+                if rule[0][1] == "<" and ra < int(rule[0][2:]):
+                    if rule[1] in ["A", "R"]:
+                        status = rule[1]
+                        break
+                    else:
+                        current_workflow = workflows[rule[1]]
+                        break
+                elif rule[0][1] == ">" and ra > int(rule[0][2:]):
+                    if rule[1] in ["A", "R"]:
+                        status = rule[1]
+                        break
+                    else:
+                        current_workflow = workflows[rule[1]]
+                        break
+
+    if status == "A":
+        for v in rating.values():
+            total_parts += v
+
+print(f"Result: {total_parts}")  # Result: 434147
---
+++ 2023/day19/part2.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+
+contents = Path(Path(__file__).parent, "input").read_text()
+workflows_lines, parts_lines = contents.split("\n\n")
+
+workflows: dict[str, list[tuple[str | None, str]]] = {}
+
+for wf_line in workflows_lines.split("\n"):
+    label, rules_line = wf_line.split("{")
+    rules_line = rules_line[:-1]
+    rules_raw = rules_line.split(",")
+    rules: list[tuple[str | None, str]] = []
+
+    for r in rules_raw:
+        data = r.split(":")
+
+        if len(data) == 2:
+            rules.append((data[0], data[1]))
+        else:
+            rules.append((None, data[0]))
+
+    workflows[label] = rules
+
+
+def constraints_to_perms(constraints: dict[str, tuple[int, int]]) -> int:
+    return (
+        (constraints["x"][1] - constraints["x"][0] + 1)
+        * (constraints["m"][1] - constraints["m"][0] + 1)
+        * (constraints["a"][1] - constraints["a"][0] + 1)
+        * (constraints["s"][1] - constraints["s"][0] + 1)
+    )
+
+
+def search_permutations(
+    constraints: dict[str, tuple[int, int]], rules: list[tuple[str | None, str]], n: int
+) -> int:
+    permutations = 0
+    constraints_copy = constraints.copy()
+
+    for rule in rules:
+        if rule[0] is None:  # Is last default rule?
+            if rule[1] == "A":
+                permutations += constraints_to_perms(constraints_copy)
+            elif rule[1] != "R":
+                permutations += search_permutations(
+                    constraints_copy, workflows[rule[1]], n + 1
+                )
+        else:
+            key = rule[0][0]
+            val_compare = int(rule[0][2:])
+            rule_constraints = constraints_copy.copy()
+
+            if rule[0][1] == "<":
+                rule_constraints[key] = (rule_constraints[key][0], val_compare - 1)
+                constraints_copy[key] = (val_compare, constraints_copy[key][1])
+            else:
+                rule_constraints[key] = (val_compare + 1, rule_constraints[key][1])
+                constraints_copy[key] = (constraints_copy[key][0], val_compare)
+
+            if rule[1] == "A":
+                permutations += constraints_to_perms(rule_constraints)
+            elif rule[1] != "R":
+                permutations += search_permutations(
+                    rule_constraints, workflows[rule[1]], n + 1
+                )
+
+    return permutations
+
+
+start: dict[str, tuple[int, int]] = {
+    "x": (1, 4000),
+    "m": (1, 4000),
+    "a": (1, 4000),
+    "s": (1, 4000),
+}
+
+result = search_permutations(start, workflows["in"], 0)
+
+print(f"Result: {result}")  # Result: 136146366355609
