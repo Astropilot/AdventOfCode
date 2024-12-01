59531c97a18d0935cdb73286c8f12a63b963dbf3
Yohann MARTIN <mimichackeur@gmail.com>
2024-12-01 23:03:03 +0100

    Day 1 of AOC 2024

---
+++ 2024/day1/part1.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+with Path(Path(__file__).parent, "input").open() as f:
+    lines_splitted = [line.split("   ") for line in f]
+
+
+left_list = sorted([int(i[0]) for i in lines_splitted])
+right_list = sorted([int(i[1]) for i in lines_splitted])
+
+distance = sum(
+    abs(left - right) for left, right in zip(left_list, right_list, strict=True)
+)
+
+print(f"Result: {distance}")  # Result: 3569916
---
+++ 2024/day1/part2.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+with Path(Path(__file__).parent, "input").open() as f:
+    lines_splitted = [line.split("   ") for line in f]
+
+left_list = [int(i[0]) for i in lines_splitted]
+right_list = [int(i[1]) for i in lines_splitted]
+
+similarity = sum(item * right_list.count(item) for item in left_list)
+
+print(f"Result: {similarity}")  # Result: 26407426
