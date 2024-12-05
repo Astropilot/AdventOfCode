0ee5e77c0877dbafd1cc8aaf2281f7390eda495f
Yohann MARTIN <mimichackeur@gmail.com>
2024-12-05 19:08:44 +0100

    Day 5 of AOC 2024

---
+++ 2024/day5/part1.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+with Path(Path(__file__).parent, "input").open() as f:
+    lines = [line.rstrip("\n") for line in f]
+
+delimiter = lines.index("")
+total = 0
+ordering_rules: list[list[str]] = [line.split("|") for line in lines[:delimiter]]
+
+for line in lines[delimiter + 1 :]:
+    update = line.split(",")
+
+    update_correct = True
+    for idx, page in enumerate(update):
+        for rule in ordering_rules:
+            if rule[0] == page and rule[1] in update:
+                idx_after = update.index(rule[1])
+                if idx > idx_after:
+                    update_correct = False
+            elif rule[1] == page and rule[0] in update:
+                idx_before = update.index(rule[0])
+                if idx < idx_before:
+                    update_correct = False
+        if update_correct is False:
+            break
+
+    if update_correct:
+        total += int(update[len(update) // 2])
+
+print(f"Result: {total}")  # Result: 4959
---
+++ 2024/day5/part2.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+with Path(Path(__file__).parent, "input").open() as f:
+    lines = [line.rstrip("\n") for line in f]
+
+delimiter = lines.index("")
+total = 0
+ordering_rules: list[list[str]] = [line.split("|") for line in lines[:delimiter]]
+
+for line in lines[delimiter + 1 :]:
+    update = line.split(",")
+    rules_for_update = list(
+        filter(lambda r: r[0] in update and r[1] in update, ordering_rules)
+    )
+    update_correct = True
+    idx = 0
+    while idx < len(update):
+        page = update[idx]
+        for rule in rules_for_update:
+            if rule[0] == page:
+                idx_after = update.index(rule[1])
+                if idx > idx_after:
+                    update_correct = False
+                    update.remove(page)
+                    update.insert(idx_after, page)
+                    idx = 0
+                    break
+
+            elif rule[1] == page:
+                idx_before = update.index(rule[0])
+                if idx < idx_before:
+                    update_correct = False
+                    update.remove(page)
+                    update.insert(idx_before + 1, page)
+                    idx = 0
+                    break
+        idx += 1
+
+    if update_correct:
+        continue
+
+    total += int(update[len(update) // 2])
+
+print(f"Result: {total}")  # Result: 4655
