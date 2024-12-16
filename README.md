7fa4123c3ff101942f8ffa293ae732682423700d
Yohann MARTIN <mimichackeur@gmail.com>
2024-12-16 20:32:56 +0100

    Day 10 part 2 of AOC 2022

--- README.md
+++ README.md
@@ -75,7 +75,7 @@ This repository contains my solutions in Python3 for the [Advent of Code](https:
 |  7  |   ⭐   |   ⭐   |   [🔗](./aoc_2022/day7)    |
 |  8  |   ⭐   |   ⭐   |   [🔗](./aoc_2022/day8)    |
 |  9  |   ⭐   |   ⭐   |   [🔗](./aoc_2022/day9)    |
-|  10 |   ⭐   |   ⌛   |   [🔗](./aoc_2022/day10)   |
+|  10 |   ⭐   |   ⭐   |   [🔗](./aoc_2022/day10)   |
 |  11 |   ⌛   |   ⌛   |   [🔗](./aoc_2022/day11)   |
 |  12 |   ⌛   |   ⌛   |   [🔗](./aoc_2022/day12)   |
 |  13 |   ⌛   |   ⌛   |   [🔗](./aoc_2022/day13)   |
---
+++ aoc_2022/day10/part2.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+with Path(Path(__file__).parent, "input").open() as f:
+    lines = [line.rstrip("\n") for line in f]
+
+register_x = 1
+last_count: int | None = None
+i_program = 0
+crt_idx = 0
+
+for cycle in range(1, 240 + 1, 1):
+    if last_count is None and i_program == len(lines):
+        print(f"[{cycle}] Early termination: program buffer empty")
+        break
+
+    if crt_idx + 1 >= register_x and crt_idx + 1 <= register_x + 2:
+        print("#", end="")
+    else:
+        print(".", end="")
+    crt_idx = (crt_idx + 1) % 40
+    if crt_idx == 0:
+        print()
+
+    if last_count is not None:
+        register_x += last_count
+        last_count = None
+        continue
+
+    if lines[i_program] == "noop":
+        i_program += 1
+        continue
+
+    last_count = int(lines[i_program].split(" ")[1])
+    i_program += 1
+
+# Result: PBZGRAZA
