0757cae5d28944311eb4d3e388dd8fe8eb4eb3d6
Yohann MARTIN <mimichackeur@gmail.com>
2024-12-16 20:04:48 +0100

    Day 10 part 1 of AOC 2022

---
+++ aoc_2022/day10/part1.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+with Path(Path(__file__).parent, "input").open() as f:
+    lines = [line.rstrip("\n") for line in f]
+
+register_x = 1
+last_count: int | None = None
+i_program = 0
+sum_signal_strength = 0
+
+for cycle in range(1, 220 + 1, 1):
+    if cycle in [20, 60, 100, 140, 180, 220]:
+        sum_signal_strength += cycle * register_x
+
+    if last_count is None and i_program == len(lines):
+        print(f"[{cycle}] Early termination: program buffer empty")
+        break
+    if last_count is not None:
+        register_x += last_count
+        last_count = None
+        continue
+
+    if lines[i_program] == "noop":
+        i_program += 1
+        continue
+
+    last_count = int(lines[i_program].split(" ")[1])
+    i_program += 1
+
+print(f"Result: {sum_signal_strength}")  # Result: 13440
