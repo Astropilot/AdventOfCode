7ce1cc40036e9049d02c5a78f26aa437062154d1
Yohann MARTIN <mimichackeur@gmail.com>
2024-12-17 23:05:23 +0100

    Day 3 of AOC 2021

---
+++ aoc_2021/day3/part1.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+with Path(Path(__file__).parent, "input").open() as f:
+    lines = [line.rstrip("\n") for line in f]
+
+gamma_rate_b = ""
+epsilon_rate_b = ""
+
+for bit_idx in range(len(lines[0])):
+    bit1 = 0
+    bit0 = 0
+    for line in lines:
+        if line[bit_idx] == "0":
+            bit0 += 1
+        else:
+            bit1 += 1
+    if bit1 > bit0:
+        gamma_rate_b += "1"
+        epsilon_rate_b += "0"
+    else:
+        gamma_rate_b += "0"
+        epsilon_rate_b += "1"
+
+result = int(gamma_rate_b, 2) * int(epsilon_rate_b, 2)
+
+print(f"Result: {result}")  # Result: 841526
---
+++ aoc_2021/day3/part2.py
@@ -0,0 +1,44 @@
+import typing as t
+from pathlib import Path
+
+with Path(Path(__file__).parent, "input").open() as f:
+    lines = [line.rstrip("\n") for line in f]
+
+
+def find_bits_criteria(idx: int, lines: list[str], oxygen: bool) -> t.Literal["0", "1"]:
+    bit0 = 0
+    bit1 = 0
+    for line in lines:
+        if line[idx] == "0":
+            bit0 += 1
+        else:
+            bit1 += 1
+    if bit0 == bit1:
+        return "1" if oxygen else "0"
+    return ("0" if bit0 > bit1 else "1") if oxygen else ("1" if bit0 > bit1 else "0")
+
+
+bit_crit_oxy = find_bits_criteria(0, lines, True)
+bit_crit_co2 = find_bits_criteria(0, lines, False)
+candidates_oxy = list(filter(lambda line: line[0] == bit_crit_oxy, lines))
+candidates_co2 = list(filter(lambda line: line[0] == bit_crit_co2, lines))
+
+for bit_idx in range(1, len(lines[0])):
+    if len(candidates_oxy) == 1 and len(candidates_co2) == 1:
+        break
+
+    bit_crit_oxy = find_bits_criteria(bit_idx, candidates_oxy, True)
+    bit_crit_co2 = find_bits_criteria(bit_idx, candidates_co2, False)
+
+    if len(candidates_oxy) > 1:
+        candidates_oxy = list(
+            filter(lambda line: line[bit_idx] == bit_crit_oxy, candidates_oxy)
+        )
+    if len(candidates_co2) > 1:
+        candidates_co2 = list(
+            filter(lambda line: line[bit_idx] == bit_crit_co2, candidates_co2)
+        )
+
+result = int(candidates_oxy[0], 2) * int(candidates_co2[0], 2)
+
+print(f"Result: {result}")  # Result: 4790390
