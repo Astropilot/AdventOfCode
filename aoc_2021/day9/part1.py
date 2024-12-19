30a63b7752c997f2c7536916b040d190c58e5702
Yohann MARTIN <mimichackeur@gmail.com>
2024-12-19 23:33:29 +0100

    Day 9 part 1 of AOC 2021

---
+++ aoc_2021/day9/part1.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+with Path(Path(__file__).parent, "input").open() as f:
+    grid = [list(map(int, line.rstrip("\n"))) for line in f]
+
+HEIGHT = len(grid)
+WIDTH = len(grid[0])
+
+total_risk_levels = 0
+for y in range(HEIGHT):
+    for x in range(WIDTH):
+        is_low = True
+        for dx, dy in ((0, -1), (1, 0), (0, 1), (-1, 0)):
+            xx = x + dx
+            yy = y + dy
+            if 0 <= xx < WIDTH and 0 <= yy < HEIGHT and grid[yy][xx] <= grid[y][x]:
+                is_low = False
+                break
+        if is_low:
+            total_risk_levels += 1 + grid[y][x]
+
+print(f"Result: {total_risk_levels}")  # Result: 545
