50341c0ff03ecf23b5e80e92cb94acad32801482
Yohann MARTIN <mimichackeur@gmail.com>
2024-12-27 13:23:59 +0100

    Day 4 of AOC 2015

---
+++ aoc_2015/day4/part2.py
@@ -0,0 +1,14 @@
+from hashlib import md5
+from pathlib import Path
+
+key = Path(Path(__file__).parent, "input").read_text()
+
+i = 0
+while True:
+    secret = key + str(i)
+    if md5(secret.encode()).hexdigest().startswith("000000"):  # noqa: S324
+        break
+
+    i += 1
+
+print(f"Result: {i}")
