2d7d457a9631c4b25239bf48b6f0a10229d3942e
Yohann MARTIN <mimichackeur@gmail.com>
2024-12-29 21:33:24 +0100

    Day 8 of AOC 2017

--- README.md
+++ README.md
@@ -212,7 +212,7 @@ This repository contains my solutions in Python3 for the [Advent of Code](https:
 |  24 |   ⌛   |   ⌛   |   [🔗](./aoc_2018/day24)   |
 |  25 |   ⌛   |   ⌛   |   [🔗](./aoc_2018/day25)   |
 
-## \[2017] 14⭐
+## \[2017] 16⭐
 
 | Day | Part 1 | Part 2 |         Source link         |
 | :-: | :----: | :----: | :-------------------------: |
@@ -223,7 +223,7 @@ This repository contains my solutions in Python3 for the [Advent of Code](https:
 |  5  |   ⭐   |   ⭐   |   [🔗](./aoc_2017/day5)    |
 |  6  |   ⭐   |   ⭐   |   [🔗](./aoc_2017/day6)    |
 |  7  |   ⭐   |   ⭐   |   [🔗](./aoc_2017/day7)    |
-|  8  |   ⌛   |   ⌛   |   [🔗](./aoc_2017/day8)    |
+|  8  |   ⭐   |   ⭐   |   [🔗](./aoc_2017/day8)    |
 |  9  |   ⌛   |   ⌛   |   [🔗](./aoc_2017/day9)    |
 |  10 |   ⌛   |   ⌛   |   [🔗](./aoc_2017/day10)   |
 |  11 |   ⌛   |   ⌛   |   [🔗](./aoc_2017/day11)   |
---
+++ aoc_2017/day8/part1.py
@@ -0,0 +1,30 @@
+import operator
+import typing as t
+from pathlib import Path
+
+with Path(Path(__file__).parent, "input").open() as f:
+    instructions = [line.rstrip("\n") for line in f]
+
+registers: dict[str, int] = {}
+CMP_FUNCS: dict[str, t.Callable[[int, int], bool]] = {
+    "<": operator.lt,
+    "<=": operator.le,
+    ">": operator.gt,
+    ">=": operator.ge,
+    "==": operator.eq,
+    "!=": operator.ne,
+}
+
+for instruction in instructions:
+    reg_out, operation, n, _, cmp_reg, cmp_op, cmp_n = instruction.split(" ")
+
+    reg_out_value = registers.setdefault(reg_out, 0)
+    cmp_reg_value = registers.setdefault(cmp_reg, 0)
+
+    if CMP_FUNCS[cmp_op](cmp_reg_value, int(cmp_n)):
+        if operation == "inc":
+            registers[reg_out] = reg_out_value + int(n)
+        elif operation == "dec":
+            registers[reg_out] = reg_out_value - int(n)
+
+print(f"Result: {max(registers.values())}")
